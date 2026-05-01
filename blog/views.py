c82c35d9ee76038c1e67f677c4adb9f182fea6d2
KayyyRueee <khutchinson22@ivytech.edu>
2026-04-30 20:06:04 -0400

    django orm

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    posts = [
-        {"title": "First post", "text": "Hello world!"},
-        {"title": "Second post", "text": "This is my second post"},
-    ]
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
