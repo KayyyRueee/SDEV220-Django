2bdbd80aa4f38b6bed7677ff553836affc7b3a75
KayyyRueee <khutchinson22@ivytech.edu>
2026-05-07 00:54:02 -0400

    mod 7 pt2

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),   # homepage
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'), #create a new post
+    path('drafts/', views.post_draft_list, name='post_draft_list'), #to create a post, but leave it in draft
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'), #publish a draft
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'), #delete
 ]
