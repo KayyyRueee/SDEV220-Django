c54052388d822c3f484d1925a7bd742c2c9df107
KayyyRueee <khutchinson22@ivytech.edu>
2026-05-07 16:49:42 -0400

    comment section

--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'), #publish a draft
     path('post/<int:pk>/remove/', views.post_remove, name='post_remove'), #delete
     path('accounts/profile/', views.profile, name='profile'), #profiles
-    path('accounts/register/', blog_views.register, name='register'),
+    path('accounts/register/', blog_views.register, name='register'), #register to make an account
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'), #add a comment to post
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required      #makes it so only logged in users can create, edit, publish, or delete posts
 from django.http import HttpResponseForbidden
 
@@ -125,4 +125,19 @@ def register(request):
         "form": form,
         "error": error
     })
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
