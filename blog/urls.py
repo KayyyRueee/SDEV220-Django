05bda02c2f894dd650f024c00b74fa85784deb3c
KayyyRueee <khutchinson22@ivytech.edu>
2026-05-07 01:59:35 -0400

    mod 7 & final project updates

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('drafts/', views.post_draft_list, name='post_draft_list'), #to create a post, but leave it in draft
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'), #publish a draft
     path('post/<int:pk>/remove/', views.post_remove, name='post_remove'), #delete
+    path('accounts/profile/', views.profile, name='profile'), #profiles
+    path('accounts/register/', views.register, name='register'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -58,3 +58,49 @@ def post_remove(request, pk):
     return redirect('post_list')
 
 
+from django.contrib.auth import login
+from .forms import RegisterForm
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = RegisterForm()
+
+    return render(request, "registration/register.html", {"form": form})
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def profile(request):
+    return render(request, "registration/profile.html")
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+VALID_COMPANY_CODES = ["CompA1", "CompB2", "CompC3"]
+
+def register(request):
+    error = None
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        code = request.POST.get("company_code")
+
+        # check company code FIRST
+        if code not in VALID_COMPANY_CODES:
+            error = "Invalid company code. You are not authorized to register."
+        elif form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/register.html", {
+        "form": form,
+        "error": error
+    })
+
